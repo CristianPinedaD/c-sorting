c0b923e8743bfcb34bca6e16caf065689663975f
CristianPinedaD <109752965+CristianPinedaD@users.noreply.github.com>
2024-11-18 11:17:03 -0500

    Add i/o

--- main.c
+++ main.c
@@ -61,8 +61,25 @@ void mergeSort(int arr[], int l, int r) {
 	}
 }
 
+// Function to print an array
+void printArray(int A[], int size) {
+	int i;
+	for (i = 0; i < size; i++)
+		printf("%d ", A[i]);
+	printf("\n");
+}
 
-int main(void) {
+// Driver code
+int main() {
+	int arr[] = {12, 11, 13, 5, 6, 7};
+	int arr_size = sizeof(arr) / sizeof(arr[0]);
 
+	printf("Given array is \n");
+	printArray(arr, arr_size);
+
+	mergeSort(arr, 0, arr_size - 1);
+
+	printf("\nSorted array is \n");
+	printArray(arr, arr_size);
 	return 0;
 }

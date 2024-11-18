d1d11c0c45ff7343777988d5b26efed9c552717e
CristianPinedaD <109752965+CristianPinedaD@users.noreply.github.com>
2024-11-18 10:37:38 -0500

    Implement mergesort

--- main.c
+++ main.c
@@ -49,9 +49,16 @@ int merge(int arr[], int l, int m, int r){
 	}
 }
 
-void mergeSort(int array[], int length){
+void mergeSort(int arr[], int l, int r) {
 
+    if (l < r) {
+		int m = l + (r - 1) / 2;
 
+		mergeSort(arr, l, m);
+		mergeSort(arr, m + 1, r);
+
+		merge(arr, l, m, r); 
+	}
 }
 
 
